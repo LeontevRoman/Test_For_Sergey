525fae70761e8f88a8dc3f0a3f00011ab3149353
Roman Leontev <roman.l.v@yandex.ru>
2022-06-06 17:01:31 +0300

    change insert data base and tests

--- Test For Sergey Project/conftest.py
+++ Test For Sergey Project/conftest.py
@@ -1,16 +1,18 @@
 import pytest
 import sqlite3
+from datetime import date
 
 
-@pytest.fixture(scope='function')
+@pytest.fixture(scope='session')
 def conn(request):
     conn = sqlite3.connect(':memory:')
     cur = conn.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS client(
-           id INT PRIMARY KEY,
-           name TEXT,
-           change_date TEXT);
-        """)
+    cur.execute("CREATE TABLE IF NOT EXISTS client(id integer PRIMARY KEY autoincrement, name TEXT, change_date TEXT);")
+
+    more_users = [('roman', date.today()), ('sergey', date.today()), ('nina', date.today()), ('dima', date.today())]
+    cur.executemany("INSERT INTO client(name, change_date) VALUES(?, ?);", more_users)
+
     conn.commit()
+    cur.execute("SELECT * FROM client")
     return conn
 
--- Test For Sergey Project/test_client_list.py
+++ Test For Sergey Project/test_client_list.py
@@ -1,50 +1,43 @@
 import sqlite3
+from datetime import date
+
 
 def test_insert(conn):
-    print('пора добавить пользователя')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM client')
 
-    leight_table = len(cur.fetchall())
     insert_list = list()
-    insert_list.append(str(leight_table + 1))
-    insert_list.append(input('Введите имя : '))
-    insert_list.append(input('Введите дату в формате ****-**-** : '))
-    print(tuple(insert_list))
-    assert cur.execute("INSERT INTO client(id, name, change_date) VALUES(?, ?, ?);", tuple(insert_list)), 'WTF???'
+    insert_list.append('mariya')
+    insert_list.append(str(date.today()))
+
+    assert cur.execute("INSERT INTO client(name, change_date) VALUES(?, ?);", tuple(insert_list)), 'WTF???'
+    print()
+    cur.execute("SELECT * FROM client")
+    print(cur.fetchall())
     print('Данные успешно добавлены')
     conn.commit()
 
 
 def test_edit(conn):
     cur = conn.cursor()
-    cur.execute('SELECT * FROM client')
-    insert_list = list()
-    n = int(input('Введите номер записи: '))
-    leight_table = cur.fetchall()
+    insert_list = ('nikolay', date.today())
 
-    insert_list.append(input('Введите новое имя : '))
-    insert_list.append(input('Введите новую дату в формате ****-**-** : '))
-    try:
-        insert_list.append(leight_table[n-1][0])
-    except IndexError:
-        print('Нет такой записи')
-    assert cur.execute("UPDATE client SET name = ?, change_date = ? WHERE id = ?", tuple(insert_list)), 'Сорян, Кривые данные'
-    print('Данные изменены')
+    assert cur.execute("UPDATE client SET name = ?, change_date = ? WHERE id = '3';", insert_list), 'Сорян, Кривые данные'
+    print()
+    print('Данные успешно изменены')
+    cur.execute("SELECT * FROM client")
+    print(cur.fetchall())
     conn.commit()
 
-def test_delete(conn):
-    print('Время удалить все!')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM client')
 
-    leight_table = len(cur.fetchall())
-    delete_y_n = input('Удаляем последнюю записть, Вы уверены y/n')
-    if delete_y_n == 'y':
-        assert cur.execute("DELETE FROM client WHERE id = ?", (leight_table, )), 'Сорян, Таблица пуста'
-        print('Последняя запись удалена')
-        conn.commit()
-    else:
-        pass
+def test_delete(conn):
+    cur = conn.cursor()
+
+    assert cur.execute("DELETE FROM client WHERE id = '4'"), 'Сорян, Таблица пуста'
+    print()
+    print('Данные успешно удалены')
+    cur.execute("SELECT * FROM client")
+    print(cur.fetchall())
+    conn.commit()
+
 
 
